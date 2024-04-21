e7ea216ba6f91f00431f14d2cdd5beddc1781c9b
Jasper Cusiel <jaspercusiel@icloud.com>
2024-04-21 20:19:26 +1000

    Update formatting and adjust wheel angular velocity to match test results

--- simulation.py
+++ simulation.py
@@ -5,6 +5,7 @@ import matplotlib.patches as patches
 from matplotlib.patches import Rectangle
 from matplotlib import transforms
 
+
 def derivatives(X_k, u_k):
     # function: derivatives
     # inputs:
@@ -64,15 +65,13 @@ def control_vector(v_r, v_l, d):
     # function: control_vector
     # inputs:
     #   v_r - velocity of the right wheel
-    #   v_l - velcocity of the left wheel
+    #   v_l - velocity of the left wheel
     #   d - width of robot
     # returns:
     #   u_k - vector of control inputs
     v = (v_r + v_l) / 2
     psi_dot = (v_r - v_l) / (2 * d)
-
     u_k = [v, psi_dot]
-
     return u_k
 
 
@@ -178,13 +177,13 @@ if __name__ == '__main__':
     ####################################################################################
     #                            Simulation Parameters                                 #
     ####################################################################################
-    dt = 1  # timestep (s)
+    dt = 0.02  # timestep (s)
     t_start = 0  # time start (s)
-    t_end = 180  # time end (s)
+    t_end = 10  # time end (s)
     t = np.arange(t_start, t_end, dt)  # List of evenly spaced timestamps
 
     # Robot physical dimensions
-    robot_length = 0.105 # m
+    robot_length = 0.105  # m
     robot_width = 0.07  # m
     """
       Distance from the bottom left of the robot to the center of mass
@@ -204,11 +203,10 @@ if __name__ == '__main__':
     th = np.arctan2(distance_between_sensors / 2, sensor_to_COM_distance)  # rad
 
     # Distance between COM and the wheel rotation axis
-    d = 0.04    # m
-
+    d = 0.04  # m
     # Maximum wheel speed
-    r_wheel = 0.025  # Wheel radius (m)
-    omega = 0.25  # Maximum wheel angular velocity (rad/s)
+    r_wheel = 0.02  # Wheel radius (m)
+    omega = (24 * np.pi) / 5  # Maximum wheel angular velocity (rad/s)
     v_max = omega * r_wheel  # m/s
 
     # Vehicle vectors
@@ -228,24 +226,24 @@ if __name__ == '__main__':
 
     # Create new figure with a single subplot
     fig, ax = plt.subplots()
+    ax.set_aspect(1)
+    plt.rcParams["font.family"] = "serif"
     plot, = ax.plot([], [], 'c--', label="Center Of Mass Path")
+    subtitle = "t = {}s with v = {}cm/s, d = {}cm, w = {}cm, l = {}cm".format(t_end, round(v_max * 100, 2), round(d * 100, 2), round(robot_width * 100, 2), round(robot_length * 100, 2))
+    plt.suptitle(t=subtitle, y=0.05, size='small')
+    fig.subplots_adjust(bottom=0.17)
 
-    # Make plot square
-    ax.set_aspect('equal', adjustable='box')
-
-    # Adjust plot limits so robot doesn't disappear off the plot
-    plt.xlim(-18, 18)
+    # Set plot limits
+    plt.xlim(-20, 20)
     plt.ylim(-10, 25)
 
     # Add labels with font
     plt.xlabel("x (cm)", fontname='Times New Roman')
     plt.ylabel("y (cm)", fontname='Times New Roman')
 
-    title = "Plot of path over t = {}s with v = {}cm/s, d = {}cm, w = {}cm, l = {}cm".format(t_end, round(v_max * 100, 2), round(d * 100, 2), round(robot_width * 100, 2), round(robot_length * 100, 2))
+    plt.title(label="Robot Center Of Mass Path", fontdict={'family': 'serif', 'color': 'black', 'size': 10}, weight='bold', pad=20)
 
-    plt.title(label=title, fontdict={'family': 'serif', 'color': 'black', 'size': 10})
-
-    # Set the font to Times New Roman
+    # Set the font to Times New Roman for axis numbers
     for tick in ax.get_xticklabels():
         tick.set_fontname("Times New Roman")
     for tick in ax.get_yticklabels():
@@ -255,7 +253,8 @@ if __name__ == '__main__':
     #                                Run Simulation                                    #
     ####################################################################################
 
-    sim_data = simulate(t, dt, X, d, r, th, ellipse_origin_x, ellipse_origin_y, ellipse_a, ellipse_b, ellipse_thickness, v_max)
+    sim_data = simulate(t, dt, X, d, r, th, ellipse_origin_x, ellipse_origin_y, ellipse_a, ellipse_b, ellipse_thickness,
+                        v_max)
 
     # Pull data from simulation and store in lists to make it easier to access
     sim_x_values = sim_data[:, 1]
@@ -276,12 +275,14 @@ if __name__ == '__main__':
     # Add in two ellipses to draw track outline
     outer_ellipse = patches.Ellipse((ellipse_origin_x * 100, ellipse_origin_y * 100),
                                     (ellipse_a * 200 + (ellipse_thickness * 100 / 2)),
-                                    (ellipse_b * 200 + (ellipse_thickness * 100 / 2)), edgecolor='None', fc='black', lw=2)
+                                    (ellipse_b * 200 + (ellipse_thickness * 100 / 2)), edgecolor='None', fc='black',
+                                    lw=2)
     ax.add_patch(outer_ellipse)
 
     inner_ellipse = patches.Ellipse((ellipse_origin_x * 100, ellipse_origin_y * 100),
                                     (ellipse_a * 200 - (ellipse_thickness * 100 / 2)),
-                                    (ellipse_b * 200 - (ellipse_thickness * 100 / 2)), edgecolor='None', fc='white', lw=2)
+                                    (ellipse_b * 200 - (ellipse_thickness * 100 / 2)), edgecolor='None', fc='white',
+                                    lw=2)
     ax.add_patch(inner_ellipse)
 
     # Add in robot outline as a rectangle
@@ -290,9 +291,13 @@ if __name__ == '__main__':
     ax.add_patch(robot_body)
 
     # Add sensor dots
-    left_sensor = patches.Circle((float(sim_sensor_left_position_x[0]*100), float(sim_sensor_left_position_y[0] * 100)), radius=0.1, color='orange', label='Left Sensor')
+    left_sensor = patches.Circle(
+        (float(sim_sensor_left_position_x[0] * 100), float(sim_sensor_left_position_y[0] * 100)), radius=0.1,
+        color='orange', label='Left Sensor')
     ax.add_patch(left_sensor)
-    right_sensor = patches.Circle((float(sim_sensor_right_position_x[0]*100), float(sim_sensor_right_position_y[0] * 100)), radius=0.1, color='green', label='Right Sensor')
+    right_sensor = patches.Circle(
+        (float(sim_sensor_right_position_x[0] * 100), float(sim_sensor_right_position_y[0] * 100)), radius=0.1,
+        color='green', label='Right Sensor')
     ax.add_patch(right_sensor)
 
     # Add center of mass marker
@@ -306,7 +311,7 @@ if __name__ == '__main__':
 
     # FFMpeg Writer Settings
     metadata = dict(title='Robot Simulation', artist='Jasper Cusiel')
-    writer = FFMpegWriter(fps=60, metadata=metadata)
+    writer = FFMpegWriter(fps=50, metadata=metadata)
 
     with writer.saving(fig, 'simulation.mp4', 300):
         for i in range(1, len(sim_x_values)):
@@ -325,7 +330,9 @@ if __name__ == '__main__':
             robot_body.set_y(float((sim_y_values[i] * 100) - COM_offset_y * 100))
 
             # Rotate rectangle
-            t = transforms.Affine2D().rotate_deg_around(robot_body.get_x() + COM_offset_x * 100, robot_body.get_y() + COM_offset_y * 100, float(np.degrees(sim_heading_angles[i-1])))
+            t = transforms.Affine2D().rotate_deg_around(robot_body.get_x() + COM_offset_x * 100,
+                                                        robot_body.get_y() + COM_offset_y * 100,
+                                                        float(np.degrees(sim_heading_angles[i - 1])))
             robot_body.set_transform(t + ax.transData)
 
             # Add data point to plot
